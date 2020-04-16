9758030d31ea6f12db29f9b3d221a9a5a2ba37b5
Hao H <jp97210069.ncue@gmail.com>
2020-04-16 15:43:43 +0800

    Update featureNormalize.m

--- featureNormalize.m
+++ featureNormalize.m
@@ -26,25 +26,25 @@ sigma = zeros(1, size(X, 2));
 % Hint: You might find the 'mean' and 'std' functions useful.
 %       
 
-% mean(X)�O���oX��������
-% X�O�x�}��,���G�|�O�C�C��������(1*�x�}�C�� ���榡)
+% mean(X)是取得X的平均值
+% X是矩陣時,結果會是每行的平均值(1*矩陣行數 的格式)
 mu = mean(X);
-% std(X)�O���oX���зǮt
-% X�O�x�}�ɸ�mean�@��
+% std(X)是取得X的標準差
+% X是矩陣時跟mean一樣
 sigma = std(X);
 
-% �N��S�x�Ȫ���� ��h ���S�x�Ȫ����� ��A���H ���S�x�Ȫ��зǮt
-% �N��N�S�x�Ȫ��d���Y��0~1
-% ���Y���S�x�Y��
-% (�d��]�i�H�Y��-1 ~ 1,�Ʀ�-1 ~ 2���i�H,�D�n�u�n�d�򤣭n�Ӥj�B�U�S�x�ȼƭȤ]���n�t�ӻ��Y�i)
+% 將原特徵值的資料 減去 此特徵值的平均 後再除以 此特徵值的標準差
+% 就能將特徵值的範圍限縮為0~1
+% 此即為特徵縮放
+% (範圍也可以縮為-1 ~ 1,甚至-1 ~ 2都可以,主要只要範圍不要太大且各特徵值數值也不要差太遠即可)
 
-% �����NX-������
-% ���M�Ƕi�Ӫ�X�榡����47*2���x�},��mean(X)�|�O1*2
-% ���L�]��x�}�C�Ƥ@�P,Octave�|�۰ʶi�������X�Ĥ@�C���ȴ�hmean(X)�Ĥ@��,X�ĤG�C��hmean(X)�ĤG��
-% �p�G��x�}�O��Ƥ@�P��Octave�@�ˤ]��۰ʹ����B�@
+% 直接將X-平均值
+% 雖然傳進來的X格式應為47*2的矩陣,而mean(X)會是1*2
+% 不過因兩矩陣行數一致,Octave會自動進行對應讓X第一行的值減去mean(X)第一項,X第二行減去mean(X)第二項
+% 如果兩矩陣是列數一致時Octave一樣也能自動對應運作
 
-% ���H�зǮt�������]�O�ۦP
-% ���L�O�n�U�����H�зǮt�ӫD�i��x�}���k,�ҥH�O�� ./
+% 除以標準差的部分也是相同
+% 不過是要各項除以標準差而非進行矩陣除法,所以是用 ./
 X_norm = (X - mean(X)) ./ std(X);
 
 
